6576343f2cf6e9ca1d4b8c0e3278997561c69b07
Pascal Knecht <pascal@addere.ch>
2025-10-31 21:10:36 +0100

    Bump to version 0.4.1

--- vl-yaml.rb
+++ vl-yaml.rb
@@ -3,9 +3,9 @@ class VlYaml < Formula
   homepage "https://github.com/vulnlog/vulnlog-poc"
   license "GPL-3.0"
 
-  url "https://github.com/vulnlog/vulnlog-poc/releases/download/v0.3.1/vl-yaml-macos-arm64"
+  url "https://github.com/vulnlog/vulnlog-poc/releases/download/v0.4.1/vl-yaml-macos-arm64"
   sha256 "2ec19ae64423aaf564bae6f99e0d6271ee959177f34d87e28db4709de41cdfdf"
-  version "0.3.1"
+  version "0.4.1"
 
   def install
     bin.install "vl-yaml-macos-arm64" => "vl-yaml"
